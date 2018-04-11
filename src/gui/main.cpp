3d23338bb91fbe444b73c0ffc8dfbf67c4f073e9
Igor Siemienowicz <igor@qprise.com>
2018-04-11 14:20:10 +0800

    qt-framework project commenting

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,6 +1,40 @@
+// TootDesk - Desktop Mastodon Client
+// Copyright (C) 2018 Igor Siemienowicz
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * \file main.cpp
+ * Entry point for the GUI executable
+ *
+ * \author Igor Siemienowicz
+ *
+ * \copyright GPL 3.0
+ */
+
 #include "mainwindow.h"
 #include <QApplication>
 
+/**
+ * @brief Entry point to the GUI executable
+ *
+ * @param argc The number of command-line arguments
+ *
+ * @param argv The vector of command-line arguments
+ *
+ * @return Non-zero on error
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -1,5 +1,30 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+// TootDesk - Desktop Mastodon Client
+// Copyright (C) 2018 Igor Siemienowicz
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * \file mainwindow.h
+ * Declares the `mainwindow` class
+ *
+ * \author Igor Siemienowicz
+ *
+ * \copyright GPL 3.0
+ */
+
+#ifndef _td_gui_mainwindow_h
+#define _td_gui_mainwindow_h
 
 #include <QMainWindow>
 
@@ -7,16 +32,34 @@ namespace Ui {
 class MainWindow;
 }
 
+/**
+ * @brief Encapsulates the main GUI window
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
+    public:
+
+    /**
+     * @brief Constructor, setting up all attributes
+     *
+     * @param parent The parent widget
+     */
     explicit MainWindow(QWidget *parent = 0);
+
+    /**
+     * @brief Destructor - destroys the internal UI structures
+     */
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
-};
+    private:
 
-#endif // MAINWINDOW_H
+    /**
+     * @brief Internal UI structures (managed by Qt framework
+     */
+    Ui::MainWindow *ui;
+
+};  // end MainWindow class
+
+#endif
